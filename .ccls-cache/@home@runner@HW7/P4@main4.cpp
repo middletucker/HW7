78ba4bdc2a0b4d0b944984cdab6b7c1bae87873b
middletucker <80505827+middletucker@users.noreply.github.com>
2022-10-11 19:41:40 +0000

    P6

---
+++ .ccls-cache/@home@runner@HW7/P4@main4.cpp
@@ -0,0 +1,17 @@
+
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n, days, 
+  
+}
+
+int SpaceJunk()
+{
+
+  
+}
---
+++ P6/main6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  int voltages[10] = {89, 75, 82, 93, 78, 95, 81, 88, 77, 82};
+  double slopes[5] = {11.62, 13.98, 18.45, 12.68, 14.76};
+  double distances[100] = {6.29, 6.95, 7.25, 7.35, 7.40, 7.42};
+  double temps[64] = {78.2, 69.6, 68.5, 83.9, 55.4, 67.0, 49.8, 58.3, 62.5, 71.6};
+  char codes[15] = {'f', 'j', 'm', 'q', 't', 'w', 'z'};
+  
+}
