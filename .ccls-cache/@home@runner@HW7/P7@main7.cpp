3ec08f7b61b81bb7b9c100bc9b70c49dc766675a
middletucker <80505827+middletucker@users.noreply.github.com>
2022-10-11 20:07:32 +0000

    p7 set up and total

--- .ccls-cache/@home@runner@HW7/P7@main7.cpp
+++ .ccls-cache/@home@runner@HW7/P7@main7.cpp
@@ -8,11 +8,28 @@ A program that stores the following resistance values in an array named resistan
 
 #include <iomanip>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+  int resistance[5] = {16, 27, 39, 56, 81};
+  double current[5], power[5];
+
+  int resistanceT = 0;
+  double currentT = 0, powerT = 0;
+
+  for (int i = 0; i < 5; i++)
+    {
+      cin >> current[i];
+
+
+      
+      resistanceT = resistanceT + resistance[i];
+      currentT = currentT + current[i];
+      powerT = powerT + power[i];
+    }
 
   
 }
