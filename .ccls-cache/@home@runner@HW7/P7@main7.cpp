1c0164fd8304fbe82c43d2ad9e21113bbb648a83
middletucker <80505827+middletucker@users.noreply.github.com>
2022-10-11 20:21:14 +0000

    P7 output

--- .ccls-cache/@home@runner@HW7/P7@main7.cpp
+++ .ccls-cache/@home@runner@HW7/P7@main7.cpp
@@ -3,33 +3,44 @@ Lillian Tucker
 10/11/22
 CSCI 130
 
-A program that stores the following resistance values in an array named resistance: 16, 27, 39, 56, and 81 with two arrays named current and power, each capable of storing five double-precision numbers. Using a for loop and a cin statement, the program accepts five user-input numbers in the current array when the program is run. The program stores the product of the values of the squares of the current array and the resistance array in the power array.
+A program that stores the following resistance values in an array named
+resistance: 16, 27, 39, 56, and 81 with two arrays named current and power, each
+capable of storing five double-precision numbers. Using a for loop and a cin
+statement, the program accepts five user-input numbers in the current array when
+the program is run. The program stores the product of the values of the squares
+of the current array and the resistance array in the power array.
 */
 
+#include <cmath>
 #include <iomanip>
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-int main()
-{
+int main() {
   int resistance[5] = {16, 27, 39, 56, 81};
   double current[5], power[5];
 
   int resistanceT = 0;
   double currentT = 0, powerT = 0;
 
-  for (int i = 0; i < 5; i++)
-    {
-      cin >> current[i];
+  for (int i = 0; i < 5; i++) {
+    cin >> current[i];
 
+    power[i] = resistance[i] * pow(current[i], 2);
 
-      
-      resistanceT = resistanceT + resistance[i];
-      currentT = currentT + current[i];
-      powerT = powerT + power[i];
-    }
+    resistanceT = resistanceT + resistance[i];
+    currentT = currentT + current[i];
+    powerT = powerT + power[i];
+  }
 
-  
+  cout << left << setw(15) << "Resistance" << setw(15) << "Current" << setw(15)
+       << "Power\n";
+
+  for (int j = 0; j < 5; j++) {
+    cout << left << setw(15) << resistance[j] << setw(15) << current[j]
+         << setw(15) << power[j] << endl;
+  }
+  cout << left << setw(15) << "Total: " << resistanceT << setw(15) << currentT
+       << setw(15) << powerT << endl;
 }
