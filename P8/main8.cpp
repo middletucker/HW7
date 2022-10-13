fe6de82d496a2896aed92418ca6b7bca572d94ce
middletucker <80505827+middletucker@users.noreply.github.com>
2022-10-13 16:26:33 +0000

    p8 attempt

--- P8/main8.cpp
+++ P8/main8.cpp
@@ -4,8 +4,10 @@ Lillian Tucker
 CSCI 130
 
 Description:
-First line of input contains two integers, N(1 <= N <= 1000), the length of the message, and C(1 <= C <= 1,000,000,000), the number from the task description above.
-The next line contains N positive integers smaller than or equal to C, the message itself.
+First line of input contains two integers, N(1 <= N <= 1000), the length of the
+message, and C(1 <= C <= 1,000,000,000), the number from the task description
+above. The next line contains N positive integers smaller than or equal to C,
+the message itself.
 */
 
 #include <iomanip>
@@ -13,37 +15,34 @@ The next line contains N positive integers smaller than or equal to C, the messa
 
 using namespace std;
 
-int main()
-{
-  int i, j, temp, N, C;
+int main() {
+  int i, j, temp, N, C, count = 0;
   int num[N];
 
   cin >> N;
   cin >> C;
 
-  for (i = 0; i < N; i++)
-    {
-      cin >> num[i];
+  for (i = 0; i < N; i++) {
+    cin >> num[i];
+    if (num[i] == C) {
+      count++;
     }
-
+  }
   int X = sizeof(num) / sizeof(num[0]);
-  
-  for (i = 0; i < N; i++)
-    {
-      temp = num[N];
-      j = i - 1;
+  for (i = 0; i < N; i++) {
+    temp = num[N];
+    j = i - 1;
 
-      while (j >= 0 && num[j] > temp)
-        {
-        
-          num[j + 1] = num[j];
-          j = j - 1;
-        }
-      num[j + 1] = temp;
+    while (j >= 0 && num[j] > temp) {
+      num[j + 1] = num[j];
+      j = j - 1;
     }
-  for (i = 0; i < N; i++)
-    {
-      cout << num[i] << " \n";
+    num[j + 1] = temp;
     }
-  
+  for (i = 0; i < count; i++) {
+    swap(num[N - count - i], num[N - i]);
+  }
+  for (i = 0; i < N; i++) {
+    cout << num[i] << " \n";
+  }
 }
